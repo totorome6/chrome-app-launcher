437df51c6f6d6783a2ceda4f3b09b7e611051e57
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2016-08-25 21:00:17 +0200

    fix: add title attr to app elements

--- app/scripts/popup/ui/appElementBuilder.js
+++ app/scripts/popup/ui/appElementBuilder.js
@@ -15,10 +15,11 @@ export class AppElementBuilder {
         let iconUrl = IconService.getIconUrl(app);
         let result = document.createElement('div');
         result.className = 'app';
+        result.setAttribute('title', app.name);
         result.setAttribute('tabindex', '1');
         result.innerHTML = '<div class="app_content">' +
             `<div class="app_icon" style="background-image: url('${ iconUrl }')" ></div>` +
-            `<div class="app_name">${ app.name }</div>` +
+            `<div class="app_name" >${ app.name }</div>` +
             '</div>';
         result.dataset.appId = app.id;
         return result;
